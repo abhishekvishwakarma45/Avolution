7ee3af868e0987e95f14c2b3f1424a5fa8e046f3
Abhishek Vishwakarma <abhishekv6281@gmail.com>
2025-02-08 14:09:13 +0530

    hero section added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import Header from "./components/Header";
 import React from "react";
-
+import HeroSection from "./components/HeroSection";
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <HeroSection />
     </Fragment>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,16 @@ import { RiMenu3Fill } from "react-icons/ri";
 const Header = () => {
   return (
     <Fragment>
-      <div className="grid w-full h-20 grid-cols-2 lg:grid-cols-3">
-        <div className="items-center justify-start flex-auto hidden w-auto h-full pl-2 pr-2 lg:flex">
-          <ul className="flex items-center justify-center w-auto h-auto font-bold text-center uppercase flexwrap">
+      <div className="grid w-full h-20 grid-cols-2 pt-1 pr-2 pb-1 pl-2  absolute  top-0 z-999 lg:grid-cols-3 ">
+        <div className="items-center justify-start flex-auto hidden w-auto h-full lg:flex">
+          <ul className="pl-4 flex items-center justify-center w-auto h-auto font-bold text-center uppercase flexwrap">
             <NavLink>
-              <li className="m-2">Men</li>
+              <li className="m-2">Home</li>
             </NavLink>
             <NavLink>
-              <li className="m-2">Women</li>
+              <li className="m-2">Categories</li>
             </NavLink>
-            <NavLink>
-              <li className="m-2">T-Shirts</li>
-            </NavLink>
+
             <NavLink>
               <li className="m-2">About</li>
             </NavLink>
@@ -26,45 +24,39 @@ const Header = () => {
             </NavLink>
           </ul>
         </div>
-        <div className="grid w-full h-20 text-4xl place-items-left lg:place-items-center logo-container">
+        <div className="grid w-[30vw] h-20 text-4xl place-items-left lg:place-items-center logo-container">
           <NavLink>
             <img
               className="object-contain w-auto h-20 mix-blend-multiply"
-              src="champion-shirt-logo.png"
+              src="logo-removebg-preview.png"
               alt=""
             />
           </NavLink>
         </div>
 
-        <div className="flex items-center justify-end w-auto h-full pr-4 lg:flex header-right-container">
-          {/* <div className="hidden m-4 lg:flex">
-            <input
-              type="text"
-              placeholder="Search anything"
-              className="p-2 pl-5 ml-4 border-r-0 lg:w-auto border-1"
-            />
-            <button className="pl-4 pr-4 mr-4 text-4xl bg-indigo-300 border-l-0 border-1">
+        <div className="flex items-center justify-end w-auto h-full pr-4 overflow-visible lg:flex header-right-container space-x-4">
+          <div className="m-3 inline-block">
+            <button className="text-3xl grid place-items-center">
               <IoIosSearch />
             </button>
-          </div> */}
-          <div>
-            <buttn className="flex items-center justify-center text-2xl ">
+          </div>
+          <div className="m-3">
+            <button className="flex items-center justify-center text-2xl">
               <HiOutlineShoppingBag />
               <span className="relative pl-2 pr-2 text-xl bg-red-400 right-3 bottom-3 rounded-4xl">
                 0
               </span>
-            </buttn>
+            </button>
           </div>
-
-          <div>
+          <div className="m-3 hidden lg:inline-block">
             <NavLink>
-              <button className="hidden pt-2 pb-2 pl-8 pr-8 font-bold text-white uppercase bg-indigo-500 lg:flex rounded-xs">
+              <button className="pt-2 pb-2 pl-8 pr-8 font-bold text-white uppercase bg-indigo-500 flex rounded-xs transition-all ease-in hover:bg-indigo-300">
                 Login
               </button>
             </NavLink>
           </div>
 
-          <div>
+          <div className="m-0">
             <NavLink>
               <button className="inline-block p-2 text-2xl font-bold lg:hidden">
                 <RiMenu3Fill />
@@ -78,5 +70,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// relative top-4 right- text-xl z-10
